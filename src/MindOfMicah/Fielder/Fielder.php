85a8812036709c8ecfdbfe9da925e2897a2b446a
micah eschbacher <micah@whamdonk.com>
2014-06-11 22:31:49 -0400

    Refactoring

--- src/MindOfMicah/Fielder/Fielder.php
+++ src/MindOfMicah/Fielder/Fielder.php
@@ -2,30 +2,48 @@
 
 namespace MindOfMicah\Fielder;
 
-use MindOfMicah\Fielder\Generators\GeneratorInterface;
 
 class Fielder
 {
+    private $field_class = 'form-control';
+
     public function text($id, $label)
     {
-        return '<div class="form-group"><label class="control-label" for="' . $id . '">' . $label . '</label><input class="form-control" type="text" name="'.$id.'" id="'.$id.'" /></div>';
+        return $this->wrap(
+            $id, 
+            $label,
+            '<input class="' . $this->field_class . '" type="text" name="' . $id . '" id="' . $id . '" />'
+        );
     }
 
     public function select($id, $label, array $options)
     {
-        $ret = '';
-        $ret.= '<div class="form-group"><label class="control-label" for="'.$id.'">'.$label.'</label><select name="'.$id.'" id="'.$id.'" class="form-control">';
+        $ret = '<select name="' . $id . '" id="' . $id . '" class="'.$this->field_class.'">';
         foreach($options as $k => $option ) {
-            $ret.='<option value="'.$k.'">'.$option.'</option>';
+            $ret .= '<option value="' . $k . '">' . $option . '</option>';
         }
-        $ret.='</select></div>';
+        $ret .= '</select>';
+
+        return $this->wrap(
+            $id,
+            $label,
+            $ret
+        );
         return $ret;
     }
 
     public function textarea($id, $label)
     {
+        return $this->wrap(
+            $id, 
+            $label, 
+            '<textarea class="' . $this->field_class . '" name="' . $id . '" id="' . $id . '"></textarea>'
+        );
 
-        return '<div class="form-group"><label class="control-label" for="' . $id . '">' . $label . '</label><textarea class="form-control" name="'.$id.'" id="'.$id.'"></textarea></div>';
+    }
 
+    private function wrap($id, $label, $field_content)
+    {
+        return '<div class="form-group"><label class="control-label" for="' . $id . '">'.$label.'</label>' . $field_content . '</div>';
     }
 }
